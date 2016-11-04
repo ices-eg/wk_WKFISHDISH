b078db9c7efde085e32f738549cea3275b20c8fc
colin millar <colinpmillar@gmail.com>
2016-11-04 15:27:16 +0100

    initial plotting script

---
+++ scripts/07_plots.R
@@ -0,0 +1,10 @@
+# -------------------------------------
+#
+# Do some plots of data and model fits
+#
+# -------------------------------------
+
+# load packages etc.
+source("scripts/header.R")
+
+
