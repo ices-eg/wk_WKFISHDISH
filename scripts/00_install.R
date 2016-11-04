890798a65313ab22376a9b3edc5eca4405dc1384
colin millar <colinpmillar@gmail.com>
2016-11-04 14:00:39 +0100

    updates header file

--- scripts/00_install.R
+++ scripts/00_install.R
@@ -5,8 +5,13 @@
 # ----------------------------
 
 # install other dependencies used in the analysis
-pkgs <- c("devtools")
+pkgs <- c("mgcv", "rgdal","rgeos","sp","spdep","raster","lattice","latticeExtra")
 if (length(pkgs)) install.packages(pkgs)
 
+
+
 # install
+# NOTE must have devtools installed
 devtools::install_github("ices-tools-prod/icesVocab")
+devtools::install_github("ices-tools-prod/icesDatras")
+devtools::install_github("faskally/gmrf")
--- scripts/header.R
+++ scripts/header.R
@@ -5,9 +5,24 @@
 #
 # ----------------------------
 
-#
+# ices webservice packages
+library(icesDatras)
 library(icesVocab)
 
+# for modelling
+library(mgcv, quietly = TRUE)
+library(gmrf, quietly = TRUE)
+
+# spatial stuff
+library(rgdal, quietly = TRUE)
+library(rgeos, quietly = TRUE)
+library(sp, quietly = TRUE)
+library(spdep, quietly = TRUE)
+library(raster, quietly = TRUE)
+
+# for plotting
+library(lattice, quietly = TRUE)
+library(latticeExtra, quietly = TRUE)
 
 
 # clear workspace -------------------
