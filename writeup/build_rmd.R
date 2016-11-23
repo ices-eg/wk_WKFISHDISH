549cb63c88f9713affa7ef5eca04d56bc3c0c382
colin millar <colinpmillar@gmail.com>
2016-11-23 09:23:07 +0100

    write docs

---
+++ writeup/build_rmd.R
@@ -0,0 +1,99 @@
+
+source("scripts/header.R")
+
+species_plots <- function(sp) {
+  gsub("%s", sp,
+"## %s
+
+```{r %s_plot, dpi=600, fig.width=7, fig.height=7, echo=FALSE}
+load('output/%s_trends.rData')
+res$p_adj <- p.adjust(res$median_p, method = 'BH')
+res %<>% filter(p_adj < 0.05) %>% select(-p)
+if (nrow(res)>0) {
+  plot_result(res, main = '%s')
+}
+```
+")
+}
+
+species_tables <- function(sp) {
+  gsub("%s", sp,
+"## %s
+
+```{r %s, results='asis', echo=FALSE}
+load('output/%s_trends.rData')
+res$p_adj <- p.adjust(res$median_p, method = 'BH')
+res %<>% filter(p_adj < 0.05) %>% select(-p)
+if (nrow(res)>0) {
+  knitr::kable(res, digits = 3)
+}
+```
+
+")
+}
+
+
+log_ratio_plots <- function(sp) {
+  gsub("%s", sp,
+"## %s
+
+```{r %s, dpi=300, fig.width=8, fig.height=7, echo=FALSE, message=FALSE}
+load('output/%s_trends.rData')
+res$p_adj <- p.adjust(res$median_p, method = 'BH')
+res %<>% filter(p_adj < 0.05) %>% select(-p)
+
+# plot log ratios
+tmp <- unnest(stocks, data)
+tmp <- left_join(res, tmp)
+if (nrow(res)>0) {
+p <- xyplot(val ~ Year | paste0(Division1, ' ~ ', Division2, ' (', round(slope,2), ')'), groups = id,
+       data = subset(tmp, id %in% 1:100),
+       type = c('p', 'r'), col = grey(0.5, alpha = 0.3), pch = 16,
+       scale = list(y = list(relation = 'free')),
+      main = '%s',
+      layout = c(4, 2), as.table = TRUE)
+print(p)
+}
+```
+
+")
+}
+
+species <- c("Spurdog", "Herring", "Sprat", "Anchovy", "Cod", "Haddock", "Whiting", "Blue Whiting",
+             "Pollack", "Saithe", "Hake", "Horse Mackerel", "Mackerel", "Megrim", "Plaice", "Sole")
+
+plots <- sapply(species, species_plots)
+tables <- sapply(species, species_tables)
+ratio_plots <- sapply(species, log_ratio_plots)
+
+cat(file = "writeup/knit_plots.Rmd",
+    readLines("writeup/plots_template.Rmd"),
+    plots, sep = "\n")
+
+cat(file = "writeup/knit_tables.Rmd",
+    readLines("writeup/tables_template.Rmd"),
+    tables, sep = "\n")
+
+cat(file = "writeup/knit_ratio_plots.Rmd",
+    readLines("writeup/tables_template.Rmd"),
+    ratio_plots, sep = "\n")
+
+
+# create a table of significant results
+resl <-
+  do.call(rbind,
+  lapply(paste0("output/", dir("output/", pattern = "*_trends.rData")),
+       function(x) {
+         load(x)
+         res$p_adj <- p.adjust(res$median_p, method = 'BH')
+         res %<>% filter(p_adj < 0.05) %>% select(-p)
+         res
+       }))
+
+res <-
+  rbind(resl[c("species", "Division1", "slope")],
+      rename(mutate(resl[c("species", "Division2", "slope")], slope=-slope), Division1 = Division2))
+
+with(res, tapply(slope, list(Division1, species), function(x) sum(sign(x))))
+
+
