c5e9b953829250faf97d8b2318cecd04a24c9d62
colin millar <colinpmillar@gmail.com>
2016-11-23 21:17:44 +0100

    update run all

--- scripts/run_all.R
+++ scripts/run_all.R
@@ -5,11 +5,18 @@
 
 source("scripts/01_download_data.R")
 
-source("scripts/02_split_data_into_species.R")
+source("scripts/02_organise_into_database.R")
 
-source("scripts/03_fit_lwr_by_species.R")
+source("scripts/03_subset_spatial_data.R")
 
 source("scripts/04_prep_for_density_model.R")
 
-source("scripts/05_fit_density_surfaces.R")
+source("scripts/05_fit_lwr_by_species.R")
 
+source("scripts/06_simulate_trends.R")
+
+source("scripts/07_run_tests.R")
+
+source("scripts/XX_centre_of_gravity.R")
+
+# Done ---------------------
