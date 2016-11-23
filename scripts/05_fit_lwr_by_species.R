2e6908b063c30a5fe6219032d0ab2d491e71ba87
colin millar <colinpmillar@gmail.com>
2016-11-23 21:17:11 +0100

    split anglerfish and ensure only one species of megrim

--- scripts/05_fit_lwr_by_species.R
+++ scripts/05_fit_lwr_by_species.R
@@ -14,10 +14,9 @@ species <- unique(fulltab$Species)
 # get aphia code function
 getAphia <- function(sp) {
   aphia <- findAphia(sp)
-  if (sp == "Anglerfish") {
-    # take black and white bellied? - if so comment out these lines
-    # the next line resticts us to white bellied only
-    aphia <- findAphia("white anglerfish")
+  if (sp == "Megrim") {
+    # take Megrim to mean Lepidorhombus whiffiagonis, Four-spot megrim is specified seperately
+    aphia <- findAphia("Lepidorhombus whiffiagonis", latin = TRUE)
   }
   aphia
 }
@@ -30,8 +29,26 @@ dbDisconnect(con)
 
 # create data.frame for results
 data <- tibble(species = unique(fulltab$Species))
-data %<>% mutate(Aphia = map(species, getAphia),
-                 Model = map2(Aphia, species, fit_lwr),
+data %<>% mutate(Aphia = map_int(species, getAphia))
+
+# check there is enough data
+data %>% mutate(nrow = map_int(Aphia, function(aphia) {
+sqlstring <-
+  paste0("SELECT * \n",
+         "FROM ca\n",
+         "WHERE Valid_Aphia IN (", paste(aphia, collapse = ", "),")\n")
+
+con <- dbConnect(SQLite(), dbname = "db/datras.sqlite")
+res <- dbSendQuery(con, sqlstring)
+ca <- dbFetch(res, n = -1)
+dbClearResult(res)
+dbDisconnect(con)
+nrow(ca)
+}))
+
+
+# fit models
+data %<>% mutate(Model = map2(Aphia, species, fit_lwr),
                  cpue = map2(Model, Aphia, predict_cpue))
 
 # some checks
